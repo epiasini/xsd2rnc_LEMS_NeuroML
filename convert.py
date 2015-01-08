c873a9d04447c5680bef5396046e3b91dbee9215
Eugenio Piasini <e.piasini@ucl.ac.uk>
2015-01-08 19:27:26 +0000

    Fix conversion of LEMS v=>0.7.3 schema.

--- convert.py
+++ convert.py
@@ -26,9 +26,10 @@ def insert_string_in_file(filename, separator, string_to_insert):
 
 def main(xsd_filename, rnc_filename=None):
     """convert LEMS/NeuroML xsd schema to rng and rnc formats"""
+    base_name = os.path.splitext(os.path.split(xsd_filename)[1])[0]
     if not rnc_filename:
-        rng_filename = os.path.splitext(os.path.split(xsd_filename)[1])[0] + ".rng"
-        rnc_filename = os.path.splitext(os.path.split(xsd_filename)[1])[0] + ".rnc"
+        rng_filename = base_name + ".rng"
+        rnc_filename = base_name + ".rnc"
     else:
         rng_filename = os.path.splitext(rnc_filename)[0] + ".rng"
 
@@ -45,20 +46,26 @@ def main(xsd_filename, rnc_filename=None):
                          shell=True)
     xsltproc_stdout = p.communicate(input=xsdtorng_resource)
     print(xsltproc_stdout[0])
+
     # patch rng file by adding the declaration of an xsi:schemaLocation attribute
     if "NeuroML" in xsd_filename:
-        definition_to_modify = "NeuroMLDocument"
+        element_to_modify = "<rng:define name=\"NeuroMLDocument\">"
     elif "LEMS" in xsd_filename:
         # insert xsi namespace declaration
         insert_string_in_file(rng_filename,
                               "<rng:grammar ",
                               "xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" ")
-        definition_to_modify ="Lems"
+        lems_version = [int(x) for x in base_name.rsplit('_v')[1].split('.')]
+        if lems_version <= [0, 7, 2]:
+            element_to_modify = "<rng:define name=\"Lems\">"
+        else:
+            element_to_modify = "<rng:element name=\"Lems\">"
     else:
         raise ValueError("This only works with LEMS or NeuroML schemas!")
 
+    
     insert_string_in_file(rng_filename,
-                          "<rng:define name=\"{}\">".format(definition_to_modify),
+                          element_to_modify,
                           "\n<rng:attribute name=\"xsi:schemaLocation\"><text/></rng:attribute>\n")
 
 
